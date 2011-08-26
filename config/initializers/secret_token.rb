7a147891fb7909725722b0525e75843ee00debd4
Jessie Wang <jessie.wang.seattle@gmail.com>
2011-08-26 12:37:33 -0700

    first app with heruko from jessie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '31bb577a922d48b9353fc50c2b679f7f2c47bd91b652943680a6e6a17fdcb24e7116c984aaa29638c0f174e118845fc531f2a4c920479944df541535229b3f73'
